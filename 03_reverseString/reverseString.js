eeacbc57accee5d330345ea6615c572bd383fb2c
Jerry He <jerryhe365@gmail.com>
2024-01-12 12:06:30 -0500

    Finished 03 Exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    let len = string.length
+    let res = ''
 
+    for (i = len - 1; i >= 0; i--) {
+        res += string[i]
+    }
+
+    return res
 };
 
 // Do not edit below this line
