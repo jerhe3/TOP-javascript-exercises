af3048dab0b39cf6a5e262ead29cebd6bdcfce16
Jerry He <jerryhe365@gmail.com>
2024-01-12 12:36:58 -0500

    Finished 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const isNumber = function (args) {
+    return typeof args === 'number'
+};
 
+const sumAll = function (x, y) {
+    let sum = 0;
+
+    if (!isNumber(x) || !isNumber(y)) {
+        return 'ERROR'
+
+    }
+
+    if (x < 0 || y < 0) return 'ERROR'
+
+    start = Math.min(x, y)
+    end = Math.max(x, y)
+
+
+    for (i = start; i <= end; i++) {
+        sum += i
+    }
+
+    return sum
 };
 
 // Do not edit below this line
