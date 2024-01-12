6030f3f54ac7f4566e34a2b4d18d42d3f8d2ed15
Jerry He <jerryhe365@gmail.com>
2024-01-12 11:56:40 -0500

    Completed 02 Exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (string, n) {
+    let res = ''
 
+    if (n < 0) return "ERROR"
+
+    for (i = 0; i < n; i++) {
+        res += string
+    }
+
+    return res
 };
 
 // Do not edit below this line
